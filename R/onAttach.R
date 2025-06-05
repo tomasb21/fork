c693963e86040acf1f8409f6f88e4b88a2b97c2f
tomasb21 <tomasb330@gmail.com>
2025-06-05 10:51:20 +0100

    Update onAttach.R
    pam

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,3 @@
 .onAttach=function(libname,pkgname){
-   packageStartupMessage("Loaded glmnet ", as.character(packageDescription("glmnet")[["Version"]]))
+   packageStartupMessage("Loaded pam ", as.character(packageDescription("pam")[["Version"]]))
 }
