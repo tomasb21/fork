e4c209c68668196b8e3c5cd31f2ff391b7190ade
tomasb21 <tomasb330@gmail.com>
2025-06-05 14:31:51 +0100

    Update base.hpp
    added mq

--- src/glmnetpp/include/glmnetpp_bits/elnet_driver/base.hpp
+++ src/glmnetpp/include/glmnetpp_bits/elnet_driver/base.hpp
@@ -6,13 +6,19 @@ namespace glmnetpp {
 
 struct ElnetDriverBase
 {
-    template <class VType>
-    void normalize_penalty(VType&& vq) const {
-        if (vq.maxCoeff() <= 0) throw util::non_positive_penalty_error();
-        vq.array() = vq.array().max(0.0);
-        vq *= vq.size() / vq.sum();
-    }
+    
+    template <class VType, class MType>
+    void normalize_penalty(VType&& vq, MType&& mq) const {
+        // Normalize vector
+        if (vq.maxCoeff() <= 0) throw util::non_positive_penalty_error();
+        vq.array() = vq.array().max(0.0);
+        vq *= vq.size() / vq.sum();
 
+        // Normalize matrix
+        if (mq.maxCoeff() <= 0) throw util::non_positive_penalty_error();
+        mq.array() = mq.array().max(0.0);
+        mq *= mq.size() / mq.sum();
+}
     template <class JDType, class JUType>
     void init_inclusion(const JDType& jd, JUType&& ju) const {
         if (jd(0) > 0) {
